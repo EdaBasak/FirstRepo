2031bb4d44e022478214b00f39798bfbe92768f2
EdaBasak <47902839+EdaBasak@users.noreply.github.com>
2019-11-28 21:54:39 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE ='FirstRepo'
+const FILES = ['/FirstRepo/', '/FirstRepo/Clock%2013_11.html', '/FirstRepo/counting%20%2B5.html', 'FirstRepo/Student%20Database.html', 'FirstRepo/cw8.html']
+
+function installCB(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
+}
+self.addEventListener('install', installCB)
+
+function save(req, resp) {
+  return caches.open(CACHE)
+  .then(cache => {
+    cache.put(req, resp.clone());
+    return resp;
+  })
+  .catch(console.log)
+}
+function fetchCB(e) { //fetch first
+  let req = e.request
+  console.log('JS_Class', req.url);
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => { return caches.match(req).then(r1 => r1) })
+  )
+}
+self.addEventListener('fetch', fetchCB)
